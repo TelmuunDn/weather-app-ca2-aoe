58dd1925d0902fc1e8fa83924dc0a2d9f07c3bb8
Luis Ramirez <146735904+LuisRamirezv@users.noreply.github.com>
2025-05-08 14:54:03 +0100

    adding more changes

--- alutech-weather/app/(tabs)/_layout.tsx
+++ alutech-weather/app/(tabs)/_layout.tsx
@@ -34,12 +34,13 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="Forecast"
         options={{
           title: 'Forecast',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      
     </Tabs>
   );
 }
